3a18f5679e1bd1d7502ab212550c3b72380c9e20
Derevyannik Sergey <33173033+dsv-sergey@users.noreply.github.com>
2018-10-28 21:00:22 +0300

    fix: test.js - fixed src/recursion.js

--- src/recursion.js
+++ src/recursion.js
@@ -4,5 +4,12 @@
  */
 module.exports = function recursion(tree) {
   const arrTree = [];
-  return tree;
+  function bst(node, height = 0) {
+    (!arrTree[height]) ? arrTree.push([]) : null;
+    arrTree[height].push(node.value);
+    (node.left) ? bst(node.left, height + 1) : null;
+    (node.right) ? bst(node.right, height + 1) : null;
+    return arrTree;
+  }
+  return bst(tree);
 };
